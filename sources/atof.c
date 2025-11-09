8a8559c1853836c8cbd29375fd35a56879c00516
Wiktoria Lisson <wilisson@3-D-12.42heilbronn.de>
2025-11-09 17:10:23 +0100

    cahnged rendering and shorten atof

--- sources/atof.c
+++ sources/atof.c
@@ -6,54 +6,46 @@
 /*   By: wilisson <wilisson@student.42heilbronn.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/08 20:25:42 by wilisson          #+#    #+#             */
-/*   Updated: 2025/11/08 20:53:09 by wilisson         ###   ########.fr       */
+/*   Updated: 2025/11/09 16:58:06 by wilisson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../fractol.h"
 
-static double	parse_fraction(const char *str)
+static double	parse_decimal(const char *str)
 {
-    double	fraction;
-    double	divisor;
+	double	decimal;
+	int		len;
 
-    fraction = 0.0;
-    divisor = 10.0;
-    while (*str >= '0' && *str <= '9')
-    {
-        fraction += (*str - '0') / divisor;
-        divisor *= 10.0;
-        str++;
-    }
-    return (fraction);
+	decimal = 0.0;
+	len = 0;
+	while (ft_isdigit(*str))
+	{
+		decimal = decimal * 10.0 + (*str - '0');
+		str++;
+		len++;
+	}
+	while (len-- > 0)
+		decimal /= 10.0;
+	return (decimal);
 }
 
 double	ft_atof(const char *str)
 {
-    double	integer;
-    double	result;
-    int		sign;
+	double	integer_part;
+	double	decimal_part;
+	int		sign;
 
-    integer = 0.0;
-    sign = 1;
-    while (*str == ' ' || (*str >= 9 && *str <= 13))
-        str++;
-    if (*str == '-' || *str == '+')
-    {
-        if (*str == '-')
-            sign = -1;
-        str++;
-    }
-    while (*str >= '0' && *str <= '9')
-    {
-        integer = integer * 10.0 + (*str - '0');
-        str++;
-    }
-    result = integer;
-    if (*str == '.')
-    {
-        str++;
-        result += parse_fraction(str);
-    }
-    return (result * sign);
+	integer_part = 0.0;
+	sign = 1;
+	while (*str == ' ' || (*str >= 9 && *str <= 13))
+		str++;
+	if (*str == '-' || *str == '+')
+		sign = (*str++ == '-') * -2 + 1;
+	while (ft_isdigit(*str))
+		integer_part = integer_part * 10.0 + (*str++ - '0');
+	decimal_part = 0.0;
+	if (*str == '.')
+		decimal_part = parse_decimal(++str);
+	return ((integer_part + decimal_part) * sign);
 }
--- sources/rendering.c
+++ sources/rendering.c
@@ -6,65 +6,44 @@
 /*   By: wilisson <wilisson@student.42heilbronn.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/07 20:35:42 by wilisson          #+#    #+#             */
-/*   Updated: 2025/11/08 20:52:18 by wilisson         ###   ########.fr       */
+/*   Updated: 2025/11/09 16:23:31 by wilisson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../fractol.h"
 
-static int	get_color(int i)
+static int	get_color(int iterations)
 {
-    int	r;
-    int	g;
-    int	b;
+	float	t;
+	int		r;
+	int		g;
+	int		b;
 
-    if (i == MAX_ITER)
-        return (0x000000FF);
-    r = (int)(sin(0.1 * i) * 127 + 128);
-    g = (int)(sin(0.1 * i + 2) * 127 + 128);
-    b = (int)(sin(0.1 * i + 4) * 127 + 128);
-    return ((r << 24) | (g << 16) | (b << 8) | 0xFF);
-}
-
-static int	calculate_pixel(t_fractal *f, int x, int y)
-{
-    t_complex	c;
-    t_complex	z;
-    int			i;
-
-    c.re = (x - WIDTH / 2.0) * 4.0 / WIDTH / f->zoom + f->offset_x;
-    c.im = (y - HEIGHT / 2.0) * 4.0 / HEIGHT / f->zoom + f->offset_y;
-    if (f->type == 1)
-    {
-        i = mandelbrot(c);
-    }
-    else
-    {
-        z.re = c.re;
-        z.im = c.im;
-        c.re = f->julia_re;
-        c.im = f->julia_im;
-        i = julia(z, c);
-    }
-    return (get_color(i));
+	if (iterations == MAX_ITER)
+		return (0x000000FF);
+	t = (float)iterations / MAX_ITER;
+	r = (int)(9 * (1 - t) * t * t * t * 255);
+	g = (int)(15 * (1 - t) * (1 - t) * t * t * 255);
+	b = (int)(8.5 * (1 - t) * (1 - t) * (1 - t) * t * 255);
+	return ((r << 24) | (g << 16) | (b << 8) | 0xFF);
 }
 
 void	render_fractal(t_fractal *f)
 {
-    int	x;
-    int	y;
-    int	color;
+	int		x;
+	int		y;
+	int		iter;
 
-    y = 0;
-    while (y < HEIGHT)
-    {
-        x = 0;
-        while (x < WIDTH)
-        {
-            color = calculate_pixel(f, x, y);
-            mlx_put_pixel(f->img, x, y, color);
-            x++;
-        }
-        y++;
-    }
+	y = 0;
+	while (y < HEIGHT)
+	{
+		x = 0;
+		while (x < WIDTH)
+		{
+			iter = calculate_pixel(f, x, y);
+			put_pixel(f, x, y, get_color(iter));
+			x++;
+		}
+		y++;
+	}
 }
